c3371f3ea300027fdd775f56c98f99535098b795
Logan Riffell <lkriffell@gmail.com>
2020-07-08 19:03:59 -0600

    Each player needs their own ships

--- test/player_test.rb
+++ test/player_test.rb
@@ -8,23 +8,29 @@ require './lib/player'
 class PlayerTest < Minitest::Test
   def test_player_and_computer_exists
     board = Board.new
-    ships = {:submarine => Ship.new("submarine", 2), :cruiser => Ship.new("Cruiser", 3)}
-    player = Player.new(board, ships, "player")
-    computer = Player.new(board, ships, "computer")
+    player_ships = {:submarine => Ship.new("submarine", 2), :cruiser => Ship.new("Cruiser", 3)}
+    player = Player.new(board, player_ships, "player")
+
+    computer_ships = {:submarine => Ship.new("submarine", 2), :cruiser => Ship.new("Cruiser", 3)}
+    computer = Player.new(board, computer_ships, "computer")
+
     assert_instance_of Player, player
     assert_instance_of Player, computer
   end
 
   def test_player_can_lose
     board = Board.new
-    ships = {:submarine => Ship.new("submarine", 2), :cruiser => Ship.new("Cruiser", 3)}
-    player = Player.new(board, ships, "player")
+    player_ships = {:submarine => Ship.new("submarine", 2), :cruiser => Ship.new("Cruiser", 3)}
+    player = Player.new(board, player_ships, "player")
     # require "pry"; binding.pry
-    ships[:submarine].hit
-    ships[:submarine].hit
-    ships[:cruiser].hit
-    ships[:cruiser].hit
-    ships[:cruiser].hit
+    player_ships[:submarine].hit
+    player_ships[:submarine].hit
+    player_ships[:cruiser].hit
+    player_ships[:cruiser].hit
+    player_ships[:cruiser].hit
+    
     assert player.has_lost?
   end
+
+
 end
